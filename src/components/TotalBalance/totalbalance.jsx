37ecbf4619aba456ddd6c6eb7e985285ab3b4f60
camila yatabe <camila.yatabe@gmail.com>
2023-11-30 10:00:20 -0300

    docs(TotalBalance): documented TotalBalance.jsx

--- src/components/TotalBalance/totalbalance.jsx
+++ src/components/TotalBalance/totalbalance.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
-import './TotalBalance.css'; // Importe o arquivo de estilos
+import './TotalBalance.css'; // Importação do arquivo de estilos
 
+// Componente TotalBalance que recebe a prop totalBalance
 const TotalBalance = ({ totalBalance }) => {
+  // Renderização do componente
   return (
     <div className="total-balance-container">
       <h2>Soma Total na Carteira</h2>
@@ -10,4 +12,4 @@ const TotalBalance = ({ totalBalance }) => {
   );
 };
 
-export default TotalBalance;
+export default TotalBalance;  // Exporta o componente TotalBalance
